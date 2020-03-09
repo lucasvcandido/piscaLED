55385d415d6f8d3d9c81937ab9ef8c4103b79f12
19180064 <19180064@DSN1271137815.cfp127aluno.local>
2020-03-09 10:59:53 -0300

    Dois LEDs alternando em 0.5s. Sem condição de acionamento.

---
+++ piscaLED.X/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#pragma config FOSC  = INTRC_NOCLKOUT   // Fonte de clock: oscilador interno
+#pragma config WDTE  = OFF              // Desabilita WatchDog Timer 
+#pragma config MCLRE = OFF              // Desab. Master Clear via pino
+#pragma config LVP   = OFF              // Desab. gravação em baixa tensão
+
+#define _XTAL_FREQ     4000000          // Freq. clock interno: 4MHz(padrão)
+
+#endif
