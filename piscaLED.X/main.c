c940039cc30616554b7e8eb39befa58dc3ce0a4a
19180064 <19180064@DSN1271137815.cfp127aluno.local>
2020-03-09 10:56:40 -0300

    Dois LEDs alternando em 0.5s. Sem condição de acionamento.

--- piscaLED.X/main.c
+++ piscaLED.X/main.c
@@ -15,6 +15,8 @@
 #define botao2 PORTDbits.RD2
 #define dirLED1 TRISDbits.TRISD0
 #define dirLED2 TRISDbits.TRISD1
+#define dirBotao2 TRISDbits.TRISD2
+#define dirBotao1 TRISDbits.TRISD3
 #define dirIn 1
 #define dirOut 0
 
@@ -24,16 +26,22 @@ void main(void) {
     LED2 = 0;
     dirLED1 = dirOut;
     dirLED2 = dirOut;
+    dirBotao1 = dirIn;
+    dirBotao2 = dirIn;
     botao1 = dirIn;
     botao2 = dirIn;
     
-    while(1){
-        LED1 = 1;
-        LED2 = 0;
-        __delay_ms(500);
-        LED1 = 0;
-        LED2 = 1;
-        
+    while(1)
+    {
+        if(!botao1&&!botao2)
+        {
+            LED1 = 1;
+            LED2 = 0;
+            __delay_ms(500);
+            LED1 = 0;
+            LED2 = 1;
+            __delay_ms(500);
+        }
     }
     
     
